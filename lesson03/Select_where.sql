01d7578c451fdca97185fb26e7e53703a6a8479b
Will Parry <lordgeralt@mail.ru>
2020-03-26 13:33:00 +0300

    Замечание исправлено

--- lesson03/Select_where.sql
+++ lesson03/Select_where.sql
@@ -1,6 +1,6 @@
 use shop;
 # Выбрать название бренда с ID = 2
-SELECT `type` FROM `brand` WHERE `id` = 3;
+SELECT `type` FROM `brand` WHERE `id` = 2;
 # Получить первые два типа товара
 SELECT `type` FROM product_type WHERE `id` <=2;
 # Получить все категории товаров со скидкой меньше 10%
