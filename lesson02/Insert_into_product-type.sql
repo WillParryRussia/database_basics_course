5e7e9312f16b49929c179209e07ddc4fcd51ae56
Will Parry <lordgeralt@mail.ru>
2020-03-26 12:06:10 +0300

    SQL: Insert into tables Brand and Product-type

---
+++ lesson02/Insert_into_product-type.sql
@@ -0,0 +1,4 @@
+use shop;
+INSERT INTO `shop`.`product_type` (`type`) VALUES ('Dress');
+INSERT INTO `shop`.`product_type` (`type`) VALUES ('T-Shirt');
+INSERT INTO `shop`.`product_type` (`type`) VALUES ('Sneakers'); 
