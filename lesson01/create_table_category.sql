aaf66de0f1a7ec07e3bf30966fb2cd1c46cb6026
WillParry <lordgeralt@mail.ru>
2020-03-26 10:14:35 +0300

    Lesson01
    SQL : Create schema and tables

---
+++ lesson01/create_table_category.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `shop`.`category` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(128) NOT NULL,
+  `discount` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
---
+++ lesson01/create_table_product-type.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `shop`.`product_type` (
+	`id` INT NOT NULL,
+    `type` VARCHAR(128) NOT NULL,
+  PRIMARY KEY (`id`));
