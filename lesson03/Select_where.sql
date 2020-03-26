1d195b1192adc0c7aef24771b1bfd70d6825be04
WillParry <lordgeralt@mail.ru>
2020-03-26 13:36:13 +0300

    Lesson03
    Lesson 03

---
+++ lesson03/Select_where.sql
@@ -0,0 +1,7 @@
+use shop;
+# Выбрать название бренда с ID = 2
+SELECT `type` FROM `brand` WHERE `id` = 2;
+# Получить первые два типа товара
+SELECT `type` FROM product_type WHERE `id` <=2;
+# Получить все категории товаров со скидкой меньше 10%
+SELECT * FROM `category` WHERE `discount` < 10;
