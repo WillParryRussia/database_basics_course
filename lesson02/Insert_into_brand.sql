25d49f1b14d9e50e485cb671c3ff90bb5abbb1ba
WillParry <lordgeralt@mail.ru>
2020-03-26 12:09:30 +0300

    Lesson02
    Lesson 02

---
+++ lesson02/Insert_into_brand.sql
@@ -0,0 +1,4 @@
+use shop;
+INSERT INTO `shop`.`brand` (`type`) VALUES ('Marco Polo');
+INSERT INTO `shop`.`brand` (`type`) VALUES ('J Lo');
+INSERT INTO `shop`.`brand` (`type`) VALUES ('Crocodile'); 
